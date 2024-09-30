e8aed777b83f5d77866e48d9b234d0c6b754e918
Aleshchenk0 <115578133+Aleshchenk0@users.noreply.github.com>
2024-09-30 10:00:09 +0300

    Add routing (#7)
    add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
   import svelteLogo from './assets/svelte.svg';
+  import Router from 'svelte-spa-router';
+  import { routes } from './routes/index.js';
   import { Badge } from 'flowbite-svelte';
 </script>
+
+<Router {routes} />
 
 <main>
   <Badge>App</Badge>
