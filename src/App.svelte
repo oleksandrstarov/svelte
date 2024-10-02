f996baf31f67afa31f6c85103f5a04d6395db888
andrii.yakovliev <andrii.yakovlie@sigma.software>
2024-10-02 11:25:32 +0300

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script>
   import Router from 'svelte-spa-router';
-  import { routes } from './routes/index';
+  import { routes } from './routes';
   import Header from './components/Header.svelte';
   import { isLoading } from 'svelte-i18n';
+  import LoadingSpinner from './components/LoadingSpinner.svelte';
 </script>
 
 <main>
   {#if !$isLoading}
     <Header />
     <Router {routes} />
+  {:else}
+    <LoadingSpinner />
   {/if}
 </main>
---
+++ src/components/LoadingSpinner.svelte
@@ -0,0 +1,14 @@
+<div class="w-screen h-screen flex justify-center items-center">
+  <div class="loader bg-primary-700 w-12 h-12 rounded-full p-2 animate-spin"></div>
+</div>
+
+<style>
+  .loader {
+    aspect-ratio: 1;
+    --_m: conic-gradient(#0000 10%, #000), linear-gradient(#000 0 0) content-box;
+    -webkit-mask: var(--_m);
+    mask: var(--_m);
+    -webkit-mask-composite: source-out;
+    mask-composite: subtract;
+  }
+</style>
