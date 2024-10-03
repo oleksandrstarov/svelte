52b3466495c2024befd3985e44a91060812e2b63
andrii.yakovliev <andrii.yakovlie@sigma.software>
2024-10-03 21:22:39 +0300

    add error handling feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,18 @@
 <script>
-  import { Spinner, Toast } from 'flowbite-svelte';
+  import { Spinner } from 'flowbite-svelte';
 
   import Router from 'svelte-spa-router';
   import { routes } from './routes';
   import Header from './components/Header.svelte';
   import { isLoading } from 'svelte-i18n';
-  import { notificationsStore } from './stores/notification';
-
-  const { closeNotification } = notificationsStore;
-
-  $: notifications = $notificationsStore.notifications;
+  import Notifications from './components/Notifications.svelte';
 </script>
 
 <main>
   {#if !$isLoading}
     <Header />
     <Router {routes} />
-    <div class="absolute bottom-0 left-0 pl-2 pb-2 w-full">
-      {#each notifications as { id, message, icon, color, count } (id)}
-        <Toast
-          class="relative border-2"
-          {color}
-          transition={() => 0}
-          on:close={() => closeNotification(id)}
-        >
-          <svelte:fragment slot="icon">
-            <span class="material-symbols-outlined"> {icon} </span>
-          </svelte:fragment>
-          {#if count > 1}
-            <div
-              class="absolute top-0 right-0 p-1 bg-white border-2 rounded-full
-             w-6 h-6 flex justify-center items-center text-xs font-bold
-             mt-[-0.5rem] mr-[-0.5rem]"
-            >
-              {count}
-            </div>
-          {/if}
-          {message}
-        </Toast>
-      {/each}
-    </div>
+    <Notifications />
   {:else}
     <div class="w-screen h-screen flex justify-center items-center">
       <Spinner />
---
+++ src/components/Notifications.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { Toast } from 'flowbite-svelte';
+
+  import { notificationsStore } from '../stores/notification';
+
+  const { closeNotification } = notificationsStore;
+
+  $: notifications = $notificationsStore.notifications;
+</script>
+
+<main>
+  <div class="absolute bottom-0 left-0 pl-2 pb-2 w-full">
+    {#each notifications as { id, message, icon, color, count } (id)}
+      <Toast
+        class="relative border-2"
+        {color}
+        transition={() => 0}
+        on:close={() => closeNotification(id)}
+      >
+        <svelte:fragment slot="icon">
+          <span class="material-symbols-outlined"> {icon} </span>
+        </svelte:fragment>
+        {#if count > 1}
+          <div
+            class="absolute top-0 right-0 p-1 bg-white border-2 rounded-full
+             w-6 h-6 flex justify-center items-center text-xs font-bold
+             mt-[-0.5rem] mr-[-0.5rem]"
+          >
+            {count}
+          </div>
+        {/if}
+        {message}
+      </Toast>
+    {/each}
+  </div>
+</main>
