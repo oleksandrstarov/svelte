11ae34d7ea221a9c997c404d3b74b655512b98cf
Avareco <31128616+Avareco@users.noreply.github.com>
2024-10-11 12:46:18 +0300

    add tests (#30)
    * add tests

---
+++ src/app.spec.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/svelte';
+import { expect, it, beforeEach, describe } from 'vitest';
+import App from './App.svelte';
+import { isLoading } from 'svelte-i18n';
+import { init, register, locale } from 'svelte-i18n';
+
+beforeEach(() => {
+  register('en', () => import('./i18n/en.json'));
+  init({ fallbackLocale: 'en' });
+  locale.set('en');
+});
+
+describe('App', () => {
+  it('should render header and notifications components when not loading', () => {
+    isLoading.set(false);
+
+    render(App);
+
+    expect(screen.getByTestId('header-element')).toBeInTheDocument();
+    expect(screen.getByTestId('notifications-element')).toBeInTheDocument();
+  });
+
+  it('should render spinner when loading', async () => {
+    isLoading.set(true);
+
+    render(App);
+
+    expect(screen.getByTestId('spinner-element')).toBeInTheDocument(); // Assuming your Spinner has a role of "status"
+  });
+});
---
+++ src/routes/forecast.spec.js
@@ -0,0 +1,45 @@
+import { render, screen, waitFor } from '@testing-library/svelte';
+import weatherService from '../services/weatherService';
+import { expect, it, vi, describe, beforeEach } from 'vitest';
+import { init, locale, register } from 'svelte-i18n';
+import ForecastPage from './ForecastPage.svelte';
+
+vi.mock('../services/weatherService');
+
+beforeEach(() => {
+  register('en', () => import('../i18n/en.json'));
+  init({ fallbackLocale: 'en' });
+  locale.set('en');
+});
+
+describe('ForecastPage', () => {
+  it('should get forecast data then display forecast', async () => {
+    const mockWeatherData = {
+      temperature: 20,
+      feelsLike: 18,
+      precipitation: 0.1,
+      weatherCode: 100,
+      windSpeed: 5,
+      windDirection: 23,
+      windGusts: 10,
+      timeOfDayId: 1,
+    };
+
+    weatherService.getWeather.mockResolvedValueOnce(mockWeatherData);
+
+    render(ForecastPage, {
+      params: { latitude: 52.52, longitude: 13.405 },
+    });
+
+    const forecastPage = screen.getByTestId('forecast-page');
+
+    expect(forecastPage).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(weatherService.getWeather).toHaveBeenCalledWith(52.52, 13.405);
+    });
+
+    expect(screen.getByTestId('forecast-main-banner')).toBeInTheDocument();
+    expect(screen.getByTestId('weekly-forecast')).toBeInTheDocument();
+  });
+});
